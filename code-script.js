528ab125f36aaea994484131b6e756966de3ab9d
Zeb Kleinsorge <zeb.kleinsorge@gmail.com>
2025-08-20 22:24:45 -0400

    code layout for mobile

--- code-script.js
+++ code-script.js
@@ -2,6 +2,7 @@
 
 //#region anagrams
 var anagrams = "\
+/// Anagram Name Generator /// <br> \
 #include <iostream> <br> \
 #include <cstring> <br> \
 #include <vector> <br> \
@@ -164,15 +165,15 @@ int main() { <br> \
 
 // #region intro
 var intro = "The following are programs from a small collection of useful string manipulation \
-tools called the word-library. Anagrams (being printed on the left) is designed to generate \
-English-style \"name-like\" caesar ciphers based on a given string. Generator, shown on the right, \
+tools called the word-library. Anagrams is designed to generate \
+English-style \"name-like\" caesar ciphers based on a given string. SecGen \
 produces passwords constructed from a set of input strings using simple substitution strategies. \
 The programs below, along with the source code for this website, can be found at github.com/zebklein"
 // #endregion
 
 // #region generator
-var generator = "/* <br> \
---DISCLAIMER-- <br> \
+var generator = "/// SecGen /// <br> \
+/* --DISCLAIMER-- <br> \
 This content is the property of David Z Kleinsorge and affiliated parites. <br> \
 The content provided herein is for educational purposes only. It is not intended to encourage or promote any illegal activities.  <br> \
 Users are reminded to use this information responsibly and in accordance with applicable laws and regulations.  <br> \
