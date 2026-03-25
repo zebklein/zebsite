82b5bb8796647a14ee4f5ae59bfd16d90db8245d
Zeb Kleinsorge <zeb.kleinsorge@gmail.com>
2026-03-25 14:28:41 -0400

    style fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   // Pass through static assets from project root
   eleventyConfig.addPassthroughCopy({ "media": "media" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
   eleventyConfig.addPassthroughCopy({ "CNAME": "CNAME" });
 
   return {
